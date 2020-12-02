add9abdfa04b8db7d179619cca1ad4ef4f4bc672
Mattias Berg <mattias.berg@gambitgroup.fi>
2020-12-02 10:59:12 +0200

    Initial repository creation with Day1 and Day2

---
+++ Day1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Day1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var expenses = LoadExpenses();
+            
+            var timer = new Stopwatch();
+            timer.Start();
+            var result1 = FirstPart(expenses);
+            timer.Stop();
+            var elapsedPart1 = timer.Elapsed;
+            
+            timer.Restart();
+            var result2 = SecondPart(expenses);
+            timer.Stop();
+
+            Console.WriteLine($"Part1 time: {elapsedPart1}");
+            Console.WriteLine($"Part2 time: {timer.Elapsed}");
+            Console.WriteLine($"Part1 result: {result1}");
+            Console.WriteLine($"Part2 result: {result2}");
+        }
+
+        private static ImmutableList<int> LoadExpenses()
+        {
+            var expensesFile = File.OpenText("expenses.txt");
+            var expenses = new List<int>();
+            string line;
+            while ((line = expensesFile.ReadLine()) != null)
+            {
+                expenses.Add(int.Parse(line));
+            }
+
+            return expenses.ToImmutableList();
+        }
+
+        private static int SecondPart(ImmutableList<int> expenses)
+        {
+            var expensesCopy = new List<int>(expenses);
+            foreach (var expense1 in expenses)
+            {
+                expensesCopy.Remove(expense1);
+                foreach (var expense2 in expensesCopy)
+                {
+                    var expense3 = expensesCopy.FirstOrDefault(e => expense1 + expense2 + e == 2020);
+                    if (expense3 == 0) continue;
+                    return expense1 * expense2 * expense3;
+                }
+            }
+
+            return 0;
+        }
+
+        private static int FirstPart(ImmutableList<int> expenses)
+        {
+            var expensesCopy = new List<int>(expenses);
+            foreach (var expense in expenses)
+            {
+                expensesCopy.Remove(expense);
+                var found = expensesCopy.FirstOrDefault(e => e + expense == 2020);
+                if (found == 0) continue;
+                return found * expense;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Day2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var passwords = ReadPasswords();
+
+            var timer = new Stopwatch();
+            timer.Start();
+            var nrValidPasswordsPart1 = FirstPart(passwords);
+            timer.Stop();
+            var elapsedPart1 = timer.Elapsed;
+
+            timer.Restart();
+            var nrValidPasswordsPart2 = SecondPart(passwords);
+            timer.Stop();
+            
+            Console.WriteLine($"Part1 time: {elapsedPart1}");
+            Console.WriteLine($"Part2 time: {timer.Elapsed}");
+            Console.WriteLine($"Part 1: {nrValidPasswordsPart1}");
+            Console.WriteLine($"Part 2: {nrValidPasswordsPart2}");
+        }
+
+        private static int SecondPart(ImmutableList<Password> passwords)
+        {
+            var nrValidPasswordsPart2 = 0;
+            foreach (var password in passwords)
+            {
+                if ((password.Pass[password.UpperBound - 1] == password.RequiredLetter ||
+                     password.Pass[password.LowerBound - 1] == password.RequiredLetter) &&
+                    !(password.Pass[password.UpperBound - 1] == password.RequiredLetter &&
+                      password.Pass[password.LowerBound - 1] == password.RequiredLetter))
+                {
+                    nrValidPasswordsPart2++;
+                }
+            }
+
+            return nrValidPasswordsPart2;
+        }
+
+        private static int FirstPart(ImmutableList<Password> passwords)
+        {
+            var nrValidPasswordsPart1 = 0;
+            foreach (var password in passwords)
+            {
+                var nrOfRequiredLetters = password.Pass.Count(p => p == password.RequiredLetter);
+                if (nrOfRequiredLetters >= password.LowerBound && nrOfRequiredLetters <= password.UpperBound)
+                {
+                    nrValidPasswordsPart1++;
+                }
+            }
+
+            return nrValidPasswordsPart1;
+        }
+
+        private static ImmutableList<Password> ReadPasswords()
+        {
+            var passwordsFile = File.OpenText("Passwords.txt");
+            var passwords = new List<Password>();
+            string line;
+            while ((line = passwordsFile.ReadLine()) != null)
+            {
+                var requirements = line.Split(": ")[0];
+                var pass = line.Split(": ")[1];
+                passwords.Add(new Password
+                {
+                    Pass = pass,
+                    LowerBound = int.Parse(requirements.Split('-')[0]),
+                    UpperBound = int.Parse(requirements.Split('-')[1].Split(' ')[0]),
+                    RequiredLetter = requirements.Split(' ')[1][0]
+                });
+            }
+
+            return passwords.ToImmutableList();
+        }
+
+        private class Password
+        {
+            public string Pass { get; set; }
+            public int UpperBound { get; set; }
+            public int LowerBound { get; set; }
+            public char RequiredLetter { get; set; }
+        }
+    }
+}

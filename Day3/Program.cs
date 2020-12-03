b90d3ecb238ac0f2d90fa4456c528c49b963559d
Mattias Berg <mattias.berg@gambitgroup.fi>
2020-12-03 09:39:54 +0200

    Day3 initial solution for part2

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using Common;
 
@@ -10,16 +12,36 @@ namespace Day3
         {
             var map = InputReader.ReadInput("input.txt", s => s);
 
+            var nrOfTrees = TraverseSlope(map, 3, 1);
+            
+            Console.WriteLine($"Part1: {nrOfTrees}");
+            
+            var traverses = new List<Tuple<int, int>>
+            {
+                new Tuple<int, int>(1,1),
+                new Tuple<int, int>(3,1),
+                new Tuple<int, int>(5,1),
+                new Tuple<int, int>(7,1),
+                new Tuple<int, int>(1,2)
+            };
+
+            var treesPerTraverse = traverses.Select(t => TraverseSlope(map, t.Item1, t.Item2));
+            var result = treesPerTraverse.Aggregate(1, (a, b) => a * b);
+            
+            Console.WriteLine($"Part2: {result}");
+        }
+
+        private static int TraverseSlope(ImmutableList<string> map, int xTraverse, int yTraverse)
+        {
             var xLength = map.First().Length;
             var yLength = map.Count;
             var currentYPos = 0;
             var currentXPos = 0;
             var nrOfTrees = 0;
-            
-            while (currentYPos < yLength-1)
+            while (currentYPos < yLength - 1)
             {
-                currentXPos += 3;
-                currentYPos += 1;
+                currentXPos += xTraverse;
+                currentYPos += yTraverse;
 
                 if (currentXPos >= xLength)
                 {
@@ -31,8 +53,8 @@ namespace Day3
                     nrOfTrees++;
                 }
             }
-            
-            Console.WriteLine($"Result: {nrOfTrees}");
+
+            return nrOfTrees;
         }
     }
 }

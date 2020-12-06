74e7907c3f86017c4df63932469a4d9671adc033
Mattias Berg <mattias.berg@gambitgroup.fi>
2020-12-06 10:15:27 +0200

    Day6 part1 initial solution

--- Common/InputReader.cs
+++ Common/InputReader.cs
@@ -7,14 +7,35 @@ namespace Common
 {
     public static class InputReader
     {
-        public static ImmutableList<T> ReadInput<T>(string path, Func<string, T> lineOperation)
+        public static ImmutableList<T> ReadInput<T>(
+            string path,
+            Func<string, T> lineOperation,
+            bool separateOnEmptyLine = false)
         {
             var file = File.OpenText(path);
             var results = new List<T>();
+            var outputLine = "";
             string line;
             while ((line = file.ReadLine()) != null)
             {
-                results.Add(lineOperation.Invoke(line));
+                if (!separateOnEmptyLine)
+                {
+                    results.Add(lineOperation(line));
+                    continue;
+                }
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    results.Add(lineOperation(outputLine));
+                    outputLine = "";
+                    continue;
+                }
+
+                outputLine += line;
+            }
+
+            if (separateOnEmptyLine)
+            {
+                results.Add(lineOperation(outputLine));
             }
 
             return results.ToImmutableList();
---
+++ Day6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using Common;
+
+namespace Day6
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var groupAnswers = InputReader.ReadInput(
+                                                "input.txt",
+                                                s => s,
+                                                true);
+
+            var result = groupAnswers
+                    .Select(s => s
+                        .Distinct()
+                        .Count())
+                    .Sum();
+            
+            Console.WriteLine($"Part1: {result}");
+        }
+    }
+}

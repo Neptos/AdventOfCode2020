1c072e772caed02dfadc293a6da36abfe6fc3d99
Mattias Berg <mattias.berg@gambitgroup.fi>
2020-12-14 11:47:43 +0200

    Day14 Part2

--- Day14/Program.cs
+++ Day14/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
 using System.Linq;
 using Common;
 
@@ -10,6 +12,106 @@ namespace Day14
         public static void Main(string[] args)
         {
             var input = InputReader.ReadInput(s => s);
+            var timer = new Stopwatch();
+            
+            timer.Start();
+            var result = Part1(input);
+            timer.Stop();
+            
+            Console.WriteLine($"Part1: {result}");
+            Console.WriteLine($"Time: {timer.Elapsed}");
+            
+            timer.Restart();
+            result = Part2(input);
+            timer.Stop();
+            
+            Console.WriteLine($"Part2: {result}");
+            Console.WriteLine($"Time: {timer.Elapsed}");
+        }
+
+        private static long Part2(ImmutableList<string> input)
+        {
+            var memory = new List<Tuple<long, long>>();
+            var orMask = "";
+            var mask = "";
+            
+            foreach (var line in input)
+            {
+                if (line.StartsWith("mask"))
+                {
+                    mask = line.Split(" ").Last();
+                    orMask = mask.Replace('X', '0');
+                }
+                else
+                {
+                    var intOrMask = Convert.ToInt64(orMask, 2);
+                    var value = long.Parse(line.Split(" ").Last());
+                    var address = long.Parse(line.Split("[")[1].Split("]")[0]);
+                    address |= intOrMask;
+                    
+                    var xPermutations = GetPermutationsWithRepetition(
+                        new List<char>{'0', '1'},
+                        mask.Count(c => c == 'X'));
+
+                    foreach (var xPermutation in xPermutations)
+                    {
+                        var newAddress = address;
+                        var xIndex = -1;
+                        foreach (var character in xPermutation)
+                        {
+                            xIndex = mask.IndexOf('X', xIndex+1);
+                            newAddress = ReplaceBinaryChar(newAddress, mask.Length - xIndex - 1, character);
+                        }
+                        
+                        if (memory.Any(m => m.Item1 == newAddress))
+                        {
+                            memory.Remove(memory.First(m => m.Item1 == newAddress));
+                        }
+
+                        memory.Add(new Tuple<long, long>(newAddress, value));
+                    }
+                }
+            }
+
+            return memory.Sum(m => m.Item2);
+        }
+        
+        private static List<List<T>> GetPermutationsWithRepetition<T>(IReadOnlyCollection<T> list, int length)
+        {
+            if (length == 1) return list.Select(t => new List<T> { t }).ToList();
+            return GetPermutationsWithRepetition(list, length - 1)
+                .SelectMany(t => list, 
+                    (t1, t2) => t1.Concat(new[] { t2 }).ToList()).ToList();
+        }
+
+        private static long ReplaceBinaryChar(long address, int index, char character)
+        {
+            var mask = CreateMask(index, character);
+            if (character == '0')
+            {
+                address &= mask;
+            }
+            else
+            {
+                address |= mask;
+            }
+
+            return address;
+        }
+
+        private static long CreateMask(int index, char character)
+        {
+            var mask = (long)Math.Pow(2, index);
+            if (character == '0')
+            {
+                return long.MaxValue - mask;
+            }
+
+            return mask;
+        }
+
+        private static long Part1(ImmutableList<string> input)
+        {
             var andMask = "";
             var orMask = "";
             var memory = new List<Tuple<int, long>>();
@@ -34,11 +136,12 @@ namespace Day14
                     {
                         memory.Remove(memory.First(m => m.Item1 == address));
                     }
+
                     memory.Add(new Tuple<int, long>(address, value));
                 }
             }
-            
-            Console.Write($"Part1: {memory.Sum(m => m.Item2)}");
+
+            return memory.Sum(m => m.Item2);
         }
     }
 }

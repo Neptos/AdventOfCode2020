17c80510e2a29d5585db38947e572f63ab39712d
Mattias Berg <mattias.berg@gambitgroup.fi>
2020-12-15 20:17:14 +0200

    Day15 solution

---
+++ Day15/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Day15
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var startingNumbers = new List<int> { 0, 5, 4, 1, 10, 14, 7 };
+            var timer = new Stopwatch();
+            
+            timer.Start();
+            var result = GetNumberSpokenFor(startingNumbers, 2020);
+            timer. Stop();
+            
+            Console.WriteLine($"Part1: {result}");
+            Console.WriteLine($"Time: {timer.Elapsed}");
+            
+            startingNumbers = new List<int>(30000000) { 0, 5, 4, 1, 10, 14, 7 };
+            
+            timer.Restart();
+            result = GetNumberSpokenFor(startingNumbers, 30000000);
+            timer. Stop();
+            
+            Console.WriteLine($"Part2: {result}");
+            Console.WriteLine($"Time: {timer.Elapsed}");
+        }
+
+        internal static int GetNumberSpokenFor(List<int> spokenNumbers, int thNumber)
+        {
+            var cache = new Dictionary<int, List<int>>();
+            for (var i = 0; i < spokenNumbers.Count; i++)
+            {
+                cache[spokenNumbers[i]] = new List<int>{ i };
+            }
+            var currentNumber = 0;
+            for (var i = spokenNumbers.Count; i < thNumber; i++)
+            {
+                var lastSpokenNumber = spokenNumbers.Last();
+                currentNumber = 0;
+
+                if (cache.ContainsKey(lastSpokenNumber) && cache[lastSpokenNumber].Count == 2)
+                {
+                    currentNumber = spokenNumbers.Count - (cache[lastSpokenNumber][0] + 1);
+                    spokenNumbers.Add(currentNumber);
+                    if (!cache.ContainsKey(currentNumber))
+                    {
+                        cache[currentNumber] = new List<int> { i };
+                        continue;
+                    }
+
+                    if (cache[currentNumber].Count == 2)
+                    {
+                        cache[currentNumber][0] = cache[currentNumber][1];
+                        cache[currentNumber][1] = i;
+                        continue;
+                    }
+                    cache[currentNumber].Add(i);
+                    continue;
+                }
+
+                if (!cache.ContainsKey(currentNumber))
+                {
+                    for (var j = spokenNumbers.Count-2; j >= 0; j--)
+                    {
+                        if (spokenNumbers[j] != lastSpokenNumber)
+                        {
+                            continue;
+                        }
+                        currentNumber = spokenNumbers.Count - (j + 1);
+                        break;
+                    }
+                    cache[currentNumber] = new List<int> { i };
+                    spokenNumbers.Add(currentNumber);
+                    continue;
+                }
+                if (cache[currentNumber].Count == 2)
+                {
+                    cache[currentNumber].RemoveAt(0);
+                }
+                cache[currentNumber].Add(i);
+                spokenNumbers.Add(currentNumber);
+            }
+
+            return currentNumber;
+        }
+    }
+}
---
+++ Day15/Test.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Day15
+{
+    public class Test
+    {
+        [Theory]
+        [MemberData(nameof(Part1Data))]
+        public void TestPart1(List<int> startingNumber, int thNumber, int expectedResult)
+        {
+            var result = Program.GetNumberSpokenFor(startingNumber, thNumber);
+            Assert.Equal(expectedResult, result);
+        }
+        
+        [Theory]
+        [MemberData(nameof(Part2Data))]
+        public void TestPart2(List<int> startingNumber, int thNumber, int expectedResult)
+        {
+            var result = Program.GetNumberSpokenFor(startingNumber, thNumber);
+            Assert.Equal(expectedResult, result);
+        }
+
+        public static IEnumerable<object[]> Part1Data =>
+            new List<object[]>
+            {
+                new object[] {new List<int> {0,3,6}, 2020, 436},
+                new object[] {new List<int> {1,3,2}, 2020, 1},
+                new object[] {new List<int> {2,1,3}, 2020, 10},
+                new object[] {new List<int> {1,2,3}, 2020, 27},
+                new object[] {new List<int> {2,3,1}, 2020, 78},
+                new object[] {new List<int> {3,2,1}, 2020, 438},
+                new object[] {new List<int> {3,1,2}, 2020, 1836}
+            };
+        
+        public static IEnumerable<object[]> Part2Data =>
+            new List<object[]>
+            {
+                new object[] {new List<int> {0,3,6}, 30000000, 175594},
+                new object[] {new List<int> {1,3,2}, 30000000, 2578},
+                new object[] {new List<int> {2,1,3}, 30000000, 3544142},
+                new object[] {new List<int> {1,2,3}, 30000000, 261214},
+                new object[] {new List<int> {2,3,1}, 30000000, 6895259},
+                new object[] {new List<int> {3,2,1}, 30000000, 18},
+                new object[] {new List<int> {3,1,2}, 30000000, 362}
+            };
+    }
+}
